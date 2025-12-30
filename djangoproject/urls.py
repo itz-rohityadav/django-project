1f2bfa46d65ac74f080e1c23eb72d25e10fc2e0b
Rohit Yadav <112674132+itz-rohityadav@users.noreply.github.com>
2025-12-30 12:52:05 +0530

    Update URL patterns for online course views

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -1,6 +1,13 @@
-from django.urls import include
+from django.urls import path
+from . import views
+
+app_name = 'onlinecourse'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('onlinecourse.urls')),
+    path('course_id/<int:course_id>/submit/', views.submit, name='submit'),
+    path(
+        'course_id/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='show_exam_result'
+    ),
 ]
