6b02391d3ce1c44c0a391ac015bf0adbf943e27d
Rohit Yadav <112674132+itz-rohityadav@users.noreply.github.com>
2025-12-30 12:47:05 +0530

    Add views for submitting and showing exam results

--- djangoproject/views.py
+++ djangoproject/views.py
@@ -1,1 +1,12 @@
+from django.shortcuts import render
+from .models import Submission
+
+def submit(request):
+    return render(request, "onlinecourse/submit.html")
+
+def show_exam_result(request):
+    score = 100
+    return render(request, "onlinecourse/result.html", {
+        "score": score
+    })
 
