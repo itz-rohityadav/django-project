ad190bd68a07d325bf62b5ad56e7c42c1c34de9e
Rohit Yadav <112674132+itz-rohityadav@users.noreply.github.com>
2025-12-30 13:15:41 +0530

    Format urlpatterns for better readability

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -4,7 +4,11 @@ from . import views
 app_name = 'onlinecourse'
 
 urlpatterns = [
-    path('course_id/<int:course_id>/submit/', views.submit, name='submit'),
+    path(
+        'course_id/<int:course_id>/submit/',
+        views.submit,
+        name='submit'
+    ),
     path(
         'course_id/<int:course_id>/submission/<int:submission_id>/result/',
         views.show_exam_result,
